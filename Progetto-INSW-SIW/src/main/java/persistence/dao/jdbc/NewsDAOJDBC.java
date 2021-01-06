7371d81b9f68d11f8d28ec03eea63f74ee79d5a7
ntl95 <fexd27@gmail.com>
2021-01-06 19:52:33 +0100

    implements "News" section (findAll() problem)

--- Progetto-INSW-SIW/src/main/java/persistence/dao/jdbc/NewsDAOJDBC.java
+++ Progetto-INSW-SIW/src/main/java/persistence/dao/jdbc/NewsDAOJDBC.java
@@ -1,28 +1,92 @@
 package persistence.dao.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 import model.NewsDTO;
+import persistence.DBSource;
 import persistence.dao.NewsDAO;
 
 public class NewsDAOJDBC implements NewsDAO {
 
+	DBSource dbSource;
+	
+	public NewsDAOJDBC(DBSource dbSource) {
+		// TODO Auto-generated constructor stub
+		this.dbSource = dbSource;
+	}
 	@Override
 	public void save(NewsDTO news) {
-		// TODO Auto-generated method stub
+		try {
+			Connection conn = dbSource.getConnection();
+			String query = "INSERT INTO news (titolo,data,username,immagine,contenuto) VALUES (?,?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setString(1, news.getTitolo());
+			statement.setDate(2, news.getData());
+			statement.setString(3, news.getUsername());
+			statement.setString(4, news.getImmagine());
+			statement.setString(5, news.getContenuto());
+			
+			statement.executeQuery();
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public NewsDTO findByPrimaryKey(String titolo) {
-		// TODO Auto-generated method stub
-		return null;
+		NewsDTO news = null;
+		try {
+			Connection conn = dbSource.getConnection();
+			PreparedStatement statement;
+			String query = "select * from news where titolo = ?";
+			statement = conn.prepareStatement(query);
+			statement.setString(1, titolo);
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				news = new NewsDTO();
+				news.setContenuto(result.getString("contenuto"));				
+				news.setData(result.getDate("data"));
+				news.setImmagine(result.getString("immagine"));
+				news.setTitolo(result.getString("titolo"));
+				news.setUsername(result.getString("username"));
+				return news;
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return news;
 	}
 
 	@Override
 	public List<NewsDTO> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<NewsDTO> newsList = new LinkedList<>();
+		try {
+			Connection connection = this.dbSource.getConnection();
+			NewsDTO news;
+			PreparedStatement statement;
+			String query = "select * from news";
+			statement = connection.prepareStatement(query);
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				news = new NewsDTO();
+				news.setContenuto(result.getString("contenuto"));				
+				news.setData(result.getDate("data"));
+				news.setImmagine(result.getString("immagine"));
+				news.setTitolo(result.getString("titolo"));
+				news.setUsername(result.getString("username"));
+				newsList.add(news);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return newsList;
 	}
 
 	@Override
