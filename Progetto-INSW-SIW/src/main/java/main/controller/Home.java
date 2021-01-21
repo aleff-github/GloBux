c975bb4098dec6c12ef5ce54efcdf141142868f3
NoNameoN-A <alessandrogreco@hacari.org>
2021-01-21 15:14:55 +0100

    Carosello sulla Home

--- Progetto-INSW-SIW/src/main/java/main/controller/Home.java
+++ Progetto-INSW-SIW/src/main/java/main/controller/Home.java
@@ -1,13 +1,23 @@
 package main.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import model.LibroDTO;
+import persistence.DBManager;
 
 @Controller
 public class Home {
 
 	@GetMapping("/")
-	public String getIndex() {
+	public String getIndex(HttpSession session) {
+		List<LibroDTO> libri = DBManager.getInstance().libroDAO().findAll();
+		System.out.println(libri.toString());
+		session.setAttribute("libri", libri);
 		return "index";
 	}
 
